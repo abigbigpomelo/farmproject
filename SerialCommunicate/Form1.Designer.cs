6949245efd9d2c32beb97ff4c13d4d78b1c7c257
unknown <huchenfei@126.com>
2016-04-07 13:58:55 +0800

    将首个版本放入github

---
+++ SerialCommunicate/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SerialCommunicate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label21 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 38400;
+            this.serialPort1.PortName = "COM8";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(107, 154);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "设置";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(95, 20);
+            this.comboBox1.TabIndex = 24;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(0, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "结束测试";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "开始测试";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label21
+            // 
+            this.label21.Font = new System.Drawing.Font("楷体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(172, 17);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(140, 46);
+            this.label21.TabIndex = 18;
+            this.label21.Text = "采样时间段";
+            this.label21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(344, 72);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(624, 431);
+            this.zedGraphControl1.TabIndex = 22;
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 10000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("楷体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(559, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 46);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "温湿度实时曲线";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(172, 72);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 431);
+            this.textBox1.TabIndex = 25;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(1034, 558);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "温湿度测试系统";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label21;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SerialCommunicate/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using ZedGraph;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Configuration;
+
+namespace SerialCommunicate
+{
+    public partial class Form1 : Form
+    {
+        byte[] senddata1 = new byte[7];
+        double tcure;
+        string wendu, shidu;
+
+        PointPairList list1 = new PointPairList();
+        PointPairList list2 = new PointPairList();
+        PointPairList list3 = new PointPairList();
+        PointPairList list4 = new PointPairList();
+        LineItem myCurve;
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            //检查是否含有串口
+            string[] str = SerialPort.GetPortNames();
+            if (str == null)
+            {
+                MessageBox.Show("本机没有串口！", "Error");
+                return;
+            }
+            
+            //添加串口项目
+            foreach (string s in System.IO.Ports.SerialPort.GetPortNames())
+            {//获取有多少个COM口
+                //System.Diagnostics.Debug.WriteLine(s);
+                comboBox1.Items.Add(s);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            senddata1[0] = 0xFD;
+            senddata1[1] = 0x03;
+            senddata1[2] = 0x28;
+            senddata1[3] = 0x7B;
+            senddata1[4] = 0xFF;
+            senddata1[5] = 0xAA;
+            senddata1[6] = 0xFF;
+
+            button1.Enabled = false;//打开串口按钮不可用
+            button2.Enabled = true;//关闭串口
+            try
+            {
+                string serialName = comboBox1.SelectedItem.ToString();
+                serialPort1.PortName = serialName;
+                serialPort1.BaudRate = 38400;
+                serialPort1.Open();
+                timer4.Start();
+                timer1.Start();
+                timer2.Start();
+            }
+            catch {
+                MessageBox.Show("端口错误,请检查串口", "错误");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);//必须手动添加事件处理程序
+
+            this.zedGraphControl1.GraphPane.Title.Text = "实时数据波形";
+            this.zedGraphControl1.GraphPane.XAxis.Title.Text = "时间";
+            this.zedGraphControl1.GraphPane.YAxis.Title.Text = "温度值";
+            this.zedGraphControl1.GraphPane.XAxis.MajorGrid.IsVisible = true;
+            this.zedGraphControl1.GraphPane.YAxis.MajorGrid.IsVisible = true;
+            this.zedGraphControl1.GraphPane.XAxis.Type = ZedGraph.AxisType.DateAsOrdinal;
+
+            DateTime dt = DateTime.Now;
+            myCurve = zedGraphControl1.GraphPane.AddCurve("温度", list1, Color.Blue, SymbolType.None);
+            this.zedGraphControl1.AxisChange();
+            this.zedGraphControl1.Refresh();
+        }
+        private List<byte> readedDataBuffer = new List<byte>();
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)//串口数据接收事件
+        {
+            lock (readedDataBuffer)
+            {
+                try
+                {
+                    if (serialPort1.BytesToRead >= 0)
+                    {
+                        byte[] data = new byte[serialPort1.BytesToRead];
+                        int count = serialPort1.Read(data, 0, data.Length);
+
+                        for (int i = 0; i < count; ++i)
+                            readedDataBuffer.Add(data[i]);
+                    }
+                }
+                catch { }
+            }
+            if (readedDataBuffer.Count >= 20)
+            {
+                int removeCount = 0;
+                for (int i = 0; i < readedDataBuffer.Count - 18; ++i)
+                {
+                        // 14 7B节点
+                        if (readedDataBuffer[i] == 0xFD & readedDataBuffer[i + 4] == 0x28 & readedDataBuffer[i + 5] == 0x7B)
+                        {
+                            byte[] data = new byte[20];
+
+                            /*节点3*/
+                            int data31, data32;
+                            double v3, T3;
+                            data31 = readedDataBuffer[i + 12];//高八位
+                            data32 = readedDataBuffer[i + 13];//低八位
+                            v3 = (data31 * 256 + data32);//电压值
+                            T3 = -39.66 + 0.01 * v3 + 0.026;
+                            tcure = T3;
+                            wendu = T3.ToString();
+                    }
+                    removeCount++;
+                }
+
+                if (readedDataBuffer.Count >= removeCount)
+                    readedDataBuffer.RemoveRange(0, removeCount);
+            }
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //sqlite 数据库
+            string uploadDate = System.DateTime.Now.ToShortDateString().ToString();
+            string uploadTime = System.DateTime.Now.ToLongTimeString().ToString();
+            SQLiteConnection conn = null;
+
+            string dbPath = "Data Source =" + "C:/sqlite" + "/数据测试.db";
+            conn = new SQLiteConnection(dbPath);//创建数据库实例，指定文件位置  
+            conn.Open();//打开数据库，若文件不存在会自动创建  
+
+            string sql = "CREATE TABLE IF NOT EXISTS '" + "温度" + uploadDate + "' (日期 varchar(6), 时间 varchar(6), 温度 varchar(6));";//建表语句  
+            SQLiteCommand cmdCreateTable = new SQLiteCommand(sql, conn);
+            cmdCreateTable.ExecuteNonQuery();//如果表不存在，创建数据表  
+
+            SQLiteCommand cmdInsert = new SQLiteCommand(conn);
+            cmdInsert.CommandText = "INSERT INTO '" + "温度" + uploadDate + "'  VALUES('" + uploadDate + "','" + uploadTime + "','" + wendu + "')";//插入几条数据  
+            cmdInsert.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+        private void label22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            try
+            {
+                timer1.Stop();
+                timer4.Stop();
+                timer2.Stop();
+                serialPort1.Close();
+            }
+            catch (Exception err)//一般情况下关闭串口不会出错，所以不需要加处理程序
+            {
+                MessageBox.Show("关闭数据出现错误", "错误提示");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            serialPort1.Write(senddata1, 0, 7);
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            zedGraphControl1.GraphPane.XAxis.Scale.MaxAuto = true;
+            this.zedGraphControl1.GraphPane.XAxis.Title.Text = "当前温度：" + wendu + " ℃" + "     时间: " + System.DateTime.Now;
+            Double x = (Double)new XDate(DateTime.Now);
+            textBox1.AppendText(DateTime.Now.ToLongTimeString() + '\n');
+
+            double y = tcure;
+            list1.Add(x, y);
+
+            if (list1.Count >= 180)
+            {
+                list1.RemoveAt(0);
+            }
+            if (list2.Count >= 180)
+            {
+                list2.RemoveAt(0);
+            }
+            if (list3.Count >= 180)
+            {
+                list3.RemoveAt(0);
+            }
+            if (list4.Count >= 180)
+            {
+                list4.RemoveAt(0);
+            }
+
+            this.zedGraphControl1.AxisChange();
+
+            this.zedGraphControl1.Refresh();
+        }
+    }
+}
